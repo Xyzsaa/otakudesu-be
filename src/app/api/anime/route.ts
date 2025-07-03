058153b12e459a3809d957b2cb7a4e99fb775a0b
Xyzsaa <114806649+Xyzsaa@users.noreply.github.com>
2025-07-03 10:03:24 +0700

    Update route.ts

--- src/app/api/anime/route.ts
+++ src/app/api/anime/route.ts
@@ -1,5 +1,17 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse, NextRequest } from "next/server";
+import anime from "@/utils/anime";
 
-export async function GET() {
-  return NextResponse.json({ data: "You need to add [:slug]" }, { status: 404 })
+export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
+  try {
+    const data = await anime(params.slug);
+
+    if (!data) {
+      return NextResponse.json({ error: "Anime not found or scraping failed." }, { status: 404 });
+    }
+
+    return NextResponse.json({ data: data }, { status: 200 });
+  } catch (error: any) {
+    console.error("Server Error:", error.message);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 }
